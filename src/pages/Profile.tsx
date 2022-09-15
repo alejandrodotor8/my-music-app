9f85ac06edf1f29fd2e2a8cba48189324ab349bc
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-09-14 22:21:23 -0500

    reset redux store at logout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '../Hooks/reduxHooks';
 import { resetUser } from '../store/slices/userSlice';
+import { resetFavoritePlaylist } from '../store/slices/favoritePlaylistSlice';
+import { resetFavorites } from '../store/slices/favoritesTracksSlice';
 import MainContent from '../components/templates/Main-content/Main-content';
 import { useAuth } from '../Hooks/useAuth';
 import Track from '../components/molecules/Track/Track';
-import Loader from '../components/atoms/Loader/Loader';
 
 export default function Profile() {
 	const { logout, isAuthenticated } = useAuth();
-	const [loading, setLoading] = useState(false);
 	const [token] = useState(localStorage.getItem('token'));
 
 	const navigate = useNavigate();
@@ -23,35 +23,33 @@ export default function Profile() {
 	const handleClick = (): void => {
 		logout();
 		dispatch(resetUser());
+		dispatch(resetFavoritePlaylist());
+		dispatch(resetFavorites());
 		navigate('/signin');
 	};
 
-	if (loading) {
-		return <Loader />;
-	} else {
-		return (
-			<MainContent
-				title={'Welcome back ' + user.name}
-				description="This are your favorites global songs"
-			>
-				{favoritesTracks && token && (
-					<ul className="tracks">
-						{favoritesTracks.map((track, index) => (
-							<Track
-								key={track.id}
-								index={index + 1}
-								track={track}
-								token={token}
-							/>
-						))}
-					</ul>
-				)}
-				{isAuthenticated && (
-					<button onClick={handleClick} className="profile__logout-btn">
-						Log out
-					</button>
-				)}
-			</MainContent>
-		);
-	}
+	return (
+		<MainContent
+			title={'Welcome back ' + user.name}
+			description="This are your favorites global songs"
+		>
+			{favoritesTracks && token && (
+				<ul className="tracks">
+					{favoritesTracks.map((track, index) => (
+						<Track
+							key={track.id}
+							index={index + 1}
+							track={track}
+							token={token}
+						/>
+					))}
+				</ul>
+			)}
+			{isAuthenticated && (
+				<button onClick={handleClick} className="profile__logout-btn">
+					Log out
+				</button>
+			)}
+		</MainContent>
+	);
 }
