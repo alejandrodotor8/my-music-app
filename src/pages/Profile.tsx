0667ee2c2d871f59e52038c937ebbefe62ae44d1
alejandrodotor8 <alejandrodotor8@gmail.com>
2022-09-15 17:39:06 -0500

    refactor logic Track

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,11 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '../Hooks/reduxHooks';
 import { resetUser } from '../store/slices/userSlice';
 import { resetFavoritePlaylist } from '../store/slices/favoritePlaylistSlice';
-import { resetFavorites } from '../store/slices/favoritesTracksSlice';
+import {
+	resetFavorites,
+	changeFav,
+} from '../store/slices/favoritesTracksSlice';
 import { useAuth } from '../Hooks/useAuth';
 import MainContent from '../components/templates/Main-content/Main-content';
 import Track from '../components/molecules/Track/Track';
 import Button from '../components/atoms/Button/Button';
+import type { ITrack } from '../shared/types';
 
 export default function Profile() {
 	const { logout, isAuthenticated } = useAuth();
@@ -21,12 +25,21 @@ export default function Profile() {
 		(state) => state.favoritesTracks.value
 	);
 
-	const handleClick = (): void => {
+	const handleClickLogout = (): void => {
 		logout();
 		dispatch(resetUser());
 		dispatch(resetFavoritePlaylist());
 		dispatch(resetFavorites());
 		navigate('/signin');
+	};
+
+	const favPlaylistID = useAppSelector(
+		(state) => state.favoritePlaylist.value.id
+	);
+	const favPlaylist = useAppSelector((state) => state.favoritesTracks.value);
+
+	const handleClickFav = (track: ITrack) => {
+		dispatch(changeFav({ track, token, favPlaylistID }));
 	};
 
 	return (
@@ -39,9 +52,10 @@ export default function Profile() {
 					{favoritesTracks.map((track, index) => (
 						<Track
 							key={track.id}
-							index={index + 1}
+							position={index + 1}
 							track={track}
-							token={token}
+							handleClick={() => handleClickFav(track)}
+							isFav={favPlaylist.some((item) => item.id === track.id)}
 						/>
 					))}
 				</ul>
@@ -49,7 +63,7 @@ export default function Profile() {
 			{isAuthenticated && (
 				<Button
 					element="button"
-					handleClick={handleClick}
+					handleClick={handleClickLogout}
 					label="Log out"
 					type="secondary"
 				/>
