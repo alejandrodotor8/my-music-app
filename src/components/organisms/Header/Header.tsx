8691389ce94a74ee94be8757dd9a15555e4ef41e
alejandrodotor8 <alejandrodotor8@gmail.com>
2022-09-16 10:10:45 -0500

    implementing flex mixing, refactoring html classes with BEM

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -8,16 +8,16 @@ function Header() {
 	const user = useAppSelector((state) => state.user.value);
 	return (
 		<header className="header">
-			<Link to="/" className="header__link--home">
-				<img src={logo} alt="logo icon" className="header__link-logo" />
-				<h1>My music</h1>
+			<Link to="/" className="header__home">
+				<img src={logo} alt="logo icon" className="header__home-logo" />
+				<h1 className="header__home-title">My music</h1>
 			</Link>
 			{user.image && (
-				<Link to={'/profile/' + user.id} className="header__link--profile">
+				<Link to={'/profile/' + user.id} className="header__profile">
 					<img
 						src={user.image}
 						alt="Profile picture"
-						className="header__profilePicture"
+						className="header__profile-picture"
 					/>
 				</Link>
 			)}
