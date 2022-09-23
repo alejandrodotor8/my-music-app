6d13e61cff9d6485c868d5c551bbd6ed5b722acc
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-09-23 16:32:52 -0500

    Path aliases config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@atoms': path.resolve(__dirname, './src/components/atoms'),
+			'@molecules': path.resolve(__dirname, './src/components/molecules'),
+			'@organisms': path.resolve(__dirname, './src/components/organisms'),
+			'@slices': path.resolve(__dirname, './src/store/slices'),
+		},
+	},
 	plugins: [react()],
 });
