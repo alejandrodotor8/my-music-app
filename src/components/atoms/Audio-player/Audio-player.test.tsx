a0336f6fe61b595636765e53d333eb2cc46b37ec
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-09-27 16:50:01 -0500

    change audio player

--- src/components/atoms/Audio-player/Audio-player.test.tsx
+++ src/components/atoms/Audio-player/Audio-player.test.tsx
@@ -2,28 +2,8 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import AudioPlayer from './Audio-player';
 
-describe('<AudioPlayer /> No Audio should:', () => {
+describe('<AudioPlayer /> should', () => {
 	const mockProps = {
-		image: 'https://i.scdn.co/image/ab67616d0000b2739b19c107109de740bad72df5',
-		audioUrl: '',
-	};
-
-	beforeEach(() => {
-		render(<AudioPlayer {...mockProps} />);
-	});
-
-	test('Render image', () => {
-		expect(screen.getByRole('img')).toBeInTheDocument();
-	});
-	test('Not render Audio and Button', () => {
-		expect(screen.queryByTestId('player-audio')).not.toBeInTheDocument();
-		expect(screen.queryByRole('button')).not.toBeInTheDocument();
-	});
-});
-
-describe('<AudioPlayer /> with Audio should', () => {
-	const mockProps = {
-		image: 'https://i.scdn.co/image/ab67616d0000b2739b19c107109de740bad72df5',
 		audioUrl:
 			'https://p.scdn.co/mp3-preview/0a51a10b22c93ee8b214fe4a87a0b37fe98687f6?cid=8c87272c262b4a81b19e595464e39c6c',
 	};
@@ -41,7 +21,7 @@ describe('<AudioPlayer /> with Audio should', () => {
 		).toBeTruthy();
 	});
 	test('Have audio src', () => {
-		expect(screen.queryByTestId('player-audio')).toHaveAttribute(
+		expect(screen.queryByTestId('player-audio-source')).toHaveAttribute(
 			'src',
 			mockProps.audioUrl
 		);
